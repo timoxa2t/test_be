cf650368f5118777c7f35220d6d07abffdbb88c0
illiarerun <ilya.vovchenko@gmail.com>
2023-06-08 16:50:10 +0300

    changed port

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
   `);
 });
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
+app.listen(443, () => {
+  console.log('Listening on port 443');
 });
 
